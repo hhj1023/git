cb1227bcd4ddde35eb478c8c45f9f898ec7c0c8d
hhj1023 <3439778015@qq.com>
2026-09-21 01:46:12 +0800

    Update excel_utils.py

--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -9,17 +9,15 @@ from config.config import EXCEL_FILE, SHEET_NAME
 
 
 def read_excel(file_path=EXCEL_FILE,sheet_name=SHEET_NAME):
-    # 相对路径：在项目根目录下执行
     workbook = openpyxl.load_workbook(file_path)
     worksheet = workbook[sheet_name]
 
     data = []
-    # 第 2 行为 key 行
     keys = [cell.value for cell in worksheet["2"]]
 
     for row in worksheet.iter_rows(min_row=3, values_only=True):
         dict_data = dict(zip(keys, row))
-        # 用例开关：is_true 为 False 的行跳过（临时禁用用例改此列即可）
+        # 用例开关，is_true 为 False 的行跳过
         if dict_data["is_true"]:
             data.append(dict_data)
 
